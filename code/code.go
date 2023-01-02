90985ee251613d46b7d282115da6021da6fbd8c6
looluo <looluo@gmail.com>
2023-01-02 15:25:43 +0800

    implement array/hash literal and index expression

--- code/code.go
+++ code/code.go
@@ -34,6 +34,11 @@ const (
 
 	OpGetGlobal
 	OpSetGlobal
+
+	OpArray
+	OpHash
+
+	OpIndex
 )
 
 type Instructions []byte
@@ -64,6 +69,9 @@ var definitions = map[Opcode]*Definition{
 	OpNull:          {"OpNull", []int{}},
 	OpGetGlobal:     {"OpGetGlobal", []int{2}},
 	OpSetGlobal:     {"OpSetGlobal", []int{2}},
+	OpArray:         {"OpArray", []int{2}},
+	OpHash:          {"OpHash", []int{2}},
+	OpIndex:         {"OpIndex", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
